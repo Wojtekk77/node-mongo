d9efb450e6dc9a95e5f5780f2f160f7cde120b1e
Wojtek Wojtkiewicz <wojtek.wojtkiewiczlt@gmail.com>
2021-04-01 08:22:50 +0200

    post method works fine

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const exphbs = require("express-handlebars");
 const {
   allowInsecurePrototypeAccess,
 } = require("@handlebars/allow-prototype-access");
-const { MongoClient } = require("mongodb");
 // const bodyparser = require("body-parser");
 
 var app = express();
@@ -22,16 +21,28 @@ app.listen(3000, () => {
 });
 
 app.get("/", (req, res) => {
-  res.send(`<h1>Hello World jup</h1>
-  <h4>click here to get access <a href="/student/list">Download</a></h4>
-  <form action="/quotes" method="POST">
-  <input type="text" placeholder="name" name="name">
-  <input type="text" placeholder="quote" name="quote">
-  <button type="submit">Submit</button>
-</form>
+  res.send(`
+    <h1>Hello World jup</h1>
+    <h4>click here to get access <a href="/student/list">Download</a></h4>
+    <form action="/quotes" method="POST">
+        <input type="text" placeholder="name" name="name">
+        <input type="text" placeholder="quote" name="quote">
+        <button type="submit">Submit</button>
+    </form>
   `);
 });
 
 app.post("/quotes", (req, res) => {
   console.log("Hellooooooooooooooooo!");
 });
+const mongoConnectionString =
+  "mongodb+srv://cluster0.uhmy8.mongodb.net/myFirstDatabase";
+const MongoClient = require("mongodb").MongoClient;
+MongoClient.connect(
+  mongoConnectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {}
+);
